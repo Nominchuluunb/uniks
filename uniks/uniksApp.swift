0c9934060b46002cfafd872ef484583c1285ec1d
Nominchuluun Baasankhuu <nominchuluun@gmail.com>
2026-06-23 16:14:23 +0200

    Initial Commit

---
+++ uniks/uniksApp.swift
@@ -0,0 +1,32 @@
+//
+//  uniksApp.swift
+//  uniks
+//
+//  Created by Nominchuluun Baasankhuu on 23.06.26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct uniksApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
